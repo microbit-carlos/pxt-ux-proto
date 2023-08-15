a974675e749966b8ec616de35ed36be35fcd7c04
Carlos Pereira Atencio <carlos@microbit.org>
2023-08-15 17:05:08 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+function isBatteryLow () {
+    return false
+}
+input.onButtonPressed(Button.AB, function () {
+    logging = !(logging)
+})
+function startLogging () {
+    basic.showIcon(IconNames.Yes)
+    while (logging) {
+        serial.writeLine("logging")
+        basic.pause(2000)
+    }
+}
+let logging = false
+logging = false
+if (isBatteryLow()) {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+            # # # # .
+            # # # # #
+            # # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # # # .
+            # . . . #
+            # # # # .
+            . . . . .
+            `)
+    }
+}
+basic.forever(function () {
+    if (logging) {
+        startLogging()
+    }
+    basic.showIcon(IconNames.No)
+})
