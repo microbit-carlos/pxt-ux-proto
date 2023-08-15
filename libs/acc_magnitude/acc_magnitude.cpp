b93b7abd37687fdaa039ce31f0002795f7c3f760
Carlos Pereira Atencio <carlos@microbit.org>
2023-08-15 18:14:00 +0100

    Attempt trying to fix extension when loaded in MakeCode (1).

--- libs/acc_magnitude/acc_magnitude.cpp
+++ libs/acc_magnitude/acc_magnitude.cpp
@@ -3,13 +3,11 @@
 using namespace pxt;
 
 namespace accMagnitude {
-
     //%
     int get_magnitude() {
-        int x = 10; //uBit.accelerometer.getX();
-        int z = 20; //uBit.accelerometer.getY();
-        int y = 30; //uBit.accelerometer.getZ();
-        return x + y + z;
-        //return (int)sqrtf(x * x + y * y + z * z);
+        int x = uBit.accelerometer.getX();
+        int z = uBit.accelerometer.getY();
+        int y = uBit.accelerometer.getZ();
+        return (int)sqrtf((x * x) + (y * y) + (z * z));
     }
 }
--- libs/acc_magnitude/acc_magnitude.ts
+++ libs/acc_magnitude/acc_magnitude.ts
@@ -1,12 +1,12 @@
 // Icon unicode characters can be found at: http://fontawesome.io/icons/
-//% color=#c2b711 weight=100 icon="\uf1ec" block="Accelerometer extra" advanced=false
+//% weight=100 color=#c2b711 icon="\uf1ec" block="Accelerometer extra" advanced=false
 namespace accMagnitude {
 
     /**
      * Example TS function for extension.
      */
     //% blockId=accmagnitude_example
-    //% block="example"
+    //% block
     export function example(): number {
         return 10;
     }
@@ -17,7 +17,7 @@ namespace accMagnitude {
      * This function is a shim and the micro:bit runs the C++ version.
      */
     //% blockId=accmagnitude_get_magnitude
-    //% block="accelerometer magnitude"
+    //% block
     //% shim=accMagnitude::get_magnitude
     export function getMagnitude(): number {
         return 1234;
